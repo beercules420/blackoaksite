975af5e63cc41227de3e69e15cc1a261406ffa7a
beercules420 <bruno.novosel@icloud.com>
2025-05-08 12:21:37 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "BlackOak d.o.o.";
+export const SITE_DESCRIPTION = "Dobro došli na web stranicu";
